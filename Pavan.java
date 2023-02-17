8b6baaf5514cdbdb284721833feb3946feffa66e
KeerthanaBVReddy <125631549+KeerthanaBVReddy@users.noreply.github.com>
2023-02-17 10:41:16 +0530

    first java repository

---
+++ Pavan.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Pavan
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a String");
+int  a = sc.nextInt();
+System.out.println("You have entered "+a);
+}
